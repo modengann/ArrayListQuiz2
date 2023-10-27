ede268a3974791ad10223961600110f64343c8f1
Moden <cmoden@gannacademy.org>
2023-10-27 09:28:00 -0400

    Update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+/**
+ * 
+ * Setup: The code is setup to take integer input from the user. The input will be
+ * added to an ArrayList called "values" until the user enters -1.
+ * 
+ * Your task:
+ * After the numbers have been collected, write code that will:
+ *   1. Find and print the average of the numbers on values
+ * 
+ */
+
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> values = new ArrayList<>();
+
+
+        while(true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input == -1){
+                break;
+            }
+            values.add(input);
+        }
+
+        System.out.println("");
+        //Write your code below here
+
+        for(Integer num : values){
+            num *= 2;
+        }
+
+        System.out.println(values);
     }
 }
